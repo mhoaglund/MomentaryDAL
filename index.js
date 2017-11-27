ef8ff98eaf8d0624c36bbd5513e15daefaf5f6d5
mhoaglund <maxwell.hoaglund@gmail.com>
2017-11-27 12:48:01 -0600

    just making a mess for now

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 'use strict';
 
-const aws = require('aws-sdk');
-const s3 = new aws.S3();
+var accessType = process.env.atype; 
+
+if(accessType === "s3"){
+    const aws = require('aws-sdk');
+    const s3 = new aws.S3();
+}
+if(accessType === "maria"){
+    var nodeMaria = require('node-mariadb');
+    var connection = nodeMaria.createConnection({
+        driverType: nodeMaria.DRIVER_TYPE_HANDLER_SOCKET,
+        host:process.env.dbhost,
+        port:8066
+      });
+      connection.on('error', function(err){
+        console.log(err);
+        process.exit(1);
+      });
+}
+
 var maxKeys = 1000;
 var fromTime = 0;
 var operation = "enumerate"
@@ -35,12 +52,39 @@ exports.handler = (event, context, callback) => {
             case "getfrom":
                 if(!fromTime) reply = "Please enclose a valid timestamp."
                 else{
-
+                    s3.getObject(params, function(err, data){
+                        if(err)callback(err)
+                        reply = data
+                        callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
+                    })
                 }
                 break
             case "getall":
                 break
             case "getlatest":
+                var list_params = {
+                    Bucket: qs["bucket"],
+                    Delimiter: '',
+                    Prefix: ''
+                }
+                s3.listObjects(list_params, function (err, data) {
+                    if(err)callback(err)
+                    data.Contents.sort(function(a, b) {
+                        return parseFloat(a.LastModified) - parseFloat(b.LastModified)
+                    })
+
+                    var get_params = {
+                        Bucket: qs["bucket"],
+                        Key: data.Contents[0].Key
+                    }
+
+                    s3.getObject(get_params, function(err, data){
+                        if(err)callback(err)
+                        reply = data
+                        callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
+                    })
+                })
+                
                 break
             case "getby":
                 if(!key) reply = "Please enclose a valid key field."
