60901002d58eaabdfaeab762b0f43ae3c29a56ed
mhoaglund <maxwell.hoaglund@gmail.com>
2017-11-27 19:10:50 -0600

    bifurcation and switching to correct maria package

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 'use strict';
 
-var accessType = process.env.atype; 
+//var accessType = process.env.atype; 
+accessType = "maria"
 
 if(accessType === "s3"){
     const aws = require('aws-sdk');
     const s3 = new aws.S3();
 }
 if(accessType === "maria"){
-    var nodeMaria = require('node-mariadb');
-    var connection = nodeMaria.createConnection({
-        driverType: nodeMaria.DRIVER_TYPE_HANDLER_SOCKET,
-        host:process.env.dbhost,
-        port:8066
-      });
-      connection.on('error', function(err){
-        console.log(err);
-        process.exit(1);
-      });
+    var Client = require('mariasql');
+    var c = new Client({
+        host: process.env.dbhost,
+        port: 8066,
+        user: 'foo',
+        password: 'bar'
+    })
+    //c.end()
 }
 
 var maxKeys = 1000;
@@ -35,56 +34,17 @@ exports.handler = (event, context, callback) => {
     } else {authenticated = true}
     if(authenticated){
         parseParams(qs)
-        switch(operation){
-            case "enumerate":
-                var params = {
-                    Bucket: qs["bucket"],
-                    Delimiter: '',
-                    Prefix: ''
-                }
-                s3.listObjects(params, function (err, data) {
-                    if(err)callback(err)
-                    console.log(data)
-                    reply = data
-                    callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
-                })
-                break
-            case "getfrom":
+        if(accessType === "maria"){
+            switch(operation){
+                case "getfrom":
                 if(!fromTime) reply = "Please enclose a valid timestamp."
                 else{
-                    s3.getObject(params, function(err, data){
-                        if(err)callback(err)
-                        reply = data
-                        callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
-                    })
+                    
                 }
                 break
             case "getall":
                 break
             case "getlatest":
-                var list_params = {
-                    Bucket: qs["bucket"],
-                    Delimiter: '',
-                    Prefix: ''
-                }
-                s3.listObjects(list_params, function (err, data) {
-                    if(err)callback(err)
-                    data.Contents.sort(function(a, b) {
-                        return parseFloat(a.LastModified) - parseFloat(b.LastModified)
-                    })
-
-                    var get_params = {
-                        Bucket: qs["bucket"],
-                        Key: data.Contents[0].Key
-                    }
-
-                    s3.getObject(get_params, function(err, data){
-                        if(err)callback(err)
-                        reply = data
-                        callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
-                    })
-                })
-                
                 break
             case "getby":
                 if(!key) reply = "Please enclose a valid key field."
@@ -92,6 +52,66 @@ exports.handler = (event, context, callback) => {
 
                 }
                 break
+            }
+        } else{
+            switch(operation){
+                case "enumerate":
+                    var params = {
+                        Bucket: qs["bucket"],
+                        Delimiter: '',
+                        Prefix: ''
+                    }
+                    s3.listObjects(params, function (err, data) {
+                        if(err)callback(err)
+                        console.log(data)
+                        reply = data
+                        callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
+                    })
+                    break
+                case "getfrom":
+                    if(!fromTime) reply = "Please enclose a valid timestamp."
+                    else{
+                        s3.getObject(params, function(err, data){
+                            if(err)callback(err)
+                            reply = data
+                            callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
+                        })
+                    }
+                    break
+                case "getall":
+                    break
+                case "getlatest":
+                    var list_params = {
+                        Bucket: qs["bucket"],
+                        Delimiter: '',
+                        Prefix: ''
+                    }
+                    s3.listObjects(list_params, function (err, data) {
+                        if(err)callback(err)
+                        data.Contents.sort(function(a, b) {
+                            return parseFloat(a.LastModified) - parseFloat(b.LastModified)
+                        })
+
+                        var get_params = {
+                            Bucket: qs["bucket"],
+                            Key: data.Contents[0].Key
+                        }
+
+                        s3.getObject(get_params, function(err, data){
+                            if(err)callback(err)
+                            reply = data
+                            callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
+                        })
+                    })
+                    
+                    break
+                case "getby":
+                    if(!key) reply = "Please enclose a valid key field."
+                    else{
+
+                    }
+                    break
+            }
         }
         callback(null, {"statusCode": 404, "body": JSON.stringify(reply)})
     }
