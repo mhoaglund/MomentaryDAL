3657e63d0ff5e1b0f8a719e9cb991c0322ba20c2
mhoaglund <maxwell.hoaglund@gmail.com>
2017-12-12 20:11:55 -0600

    large swathes of cleanup and reorient type shit

--- index.js
+++ index.js
@@ -1,152 +1,79 @@
 'use strict';
 
-//var accessType = process.env.accesstype; 
-var accessType = "db"
-
-if(accessType === "s3"){
-    const aws = require('aws-sdk');
-    const s3 = new aws.S3();
-}
-if(accessType === "db"){
-    var mysql      = require('mysql');
-    var connection = mysql.createConnection({
-        host     : process.env.dbhost,
-        port: 3306,
-        user     : process.env.dbuser,
-        password : process.env.dbpw,
-        database : process.env.dbname
-      });
-    connection.connect();
-
-    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results[0].solution);
-      });
-
-    var co_columns = "CREATE TABLE chorders (id VARCHAR(255), timestamp VARCHAR(255), author VARCHAR(255), idcolor VARCHAR(255), moves TEXT, rules TEXT, thesis TEXT)"
-      
-    connection.query(co_columns, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-    });   
-
-    connection.end();
-}
+var mysql = require('mysql');
 
 var maxKeys = 1000;
 var fromTime = 0;
-var operation = "enumerate"
+var operation = "insert"
 var key = null
 var authenticated = false;
 
 exports.handler = (event, context, callback) => {
-    //console.log(event);
     var reply = ""
-    var qs = event
     console.log(event)
-    if(!event["auth"] === "TS"){
+    if(event["auth"] != "TS"){
         var reply = "Authentication Failed."
+        callback(null, {"statusCode": 404, "body": JSON.stringify(reply)})
         return;
     } else {authenticated = true}
     if(authenticated){
         parseParams(event)
-        if(accessType === "maria"){
-            if(!c.ready()) console.log("Unable to connect!")
-            switch(operation){
-            case "insert":
-                var record = {
-                    id: 0
-                    //TODO parse post body
-                };
-
-                //like so
-                             
-                var query = connection.query('INSERT INTO chorders SET ?', record,
-                    function(err, result) {
-                        if(!err) console.log(result)
-                        else console.log(err)
-                });
-            case "getfrom":
-                if(!fromTime) reply = "Please enclose a valid timestamp."
-                else{
-                    
-                }
-                break
-            case "getall":
-                break
-            case "getlatest":
-                break
-            case "getby":
-                if(!key) reply = "Please enclose a valid key field."
-                else{
-
-                }
-                break
+        var connection = mysql.createConnection({
+            host     : process.env.dbhost,
+            port: 3306,
+            user     : process.env.dbuser,
+            password : process.env.dbpw,
+            database : process.env.dbname
+        });
+        connection.connect(function(err){
+        if (err) {
+                reply = 'error connecting: ' + err.stack
+                console.log(reply)
+                callback(null, {"statusCode": 404, "body": JSON.stringify(reply)})
             }
-        } else{
+        console.log('db connection open')
+        reply = "Business as Usual"
             switch(operation){
-                case "enumerate":
-                    var params = {
-                        Bucket: event["bucket"],
-                        Delimiter: '',
-                        Prefix: ''
-                    }
-                    s3.listObjects(params, function (err, data) {
-                        if(err)callback(err)
-                        console.log(data)
-                        reply = data
-                        callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
-                    })
-                    break
+                case "insert":
+                    var record = {
+                        id: 0
+                        //TODO parse post body
+                    };
+                    // var query = connection.query('INSERT INTO ch_orders SET ?', record,
+                    //     function(err, result) {
+                    //         if(!err) console.log(result)
+                    //         else console.log(err)
+                    // });
+                    reply = "Insert successful"
+                    wrapUp(connection, reply, 200, callback)
+                    break;
                 case "getfrom":
                     if(!fromTime) reply = "Please enclose a valid timestamp."
                     else{
-                        s3.getObject(params, function(err, data){
-                            if(err)callback(err)
-                            reply = data
-                            callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
-                        })
+                        
                     }
-                    break
+                    break;
                 case "getall":
-                    break
+                    break;
                 case "getlatest":
-                    var list_params = {
-                        Bucket: qs["bucket"],
-                        Delimiter: '',
-                        Prefix: ''
-                    }
-                    s3.listObjects(list_params, function (err, data) {
-                        if(err)callback(err)
-                        data.Contents.sort(function(a, b) {
-                            return parseFloat(a.LastModified) - parseFloat(b.LastModified)
-                        })
-
-                        var get_params = {
-                            Bucket: qs["bucket"],
-                            Key: data.Contents[0].Key
-                        }
-
-                        s3.getObject(get_params, function(err, data){
-                            if(err)callback(err)
-                            reply = data
-                            callback(null, {"statusCode": 200, "body": JSON.stringify(reply)})
-                        })
-                    })
-                    
-                    break
+                    break;
                 case "getby":
                     if(!key) reply = "Please enclose a valid key field."
                     else{
-
+        
                     }
-                    break
-            }
-        }
-        callback(null, {"statusCode": 404, "body": JSON.stringify(reply)})
+                    break;
+                }
+            wrapUp(connection, reply, 200, callback)
+        });
     }
-    
 };
+
+function wrapUp(connection, reply, code, callback){
+    connection.end(function(err) {
+        callback(null, {"statusCode": code, "body": JSON.stringify(reply)})
+    });
+}
 
 function parseParams(qstring){
     if(qstring["items"] < maxKeys) maxKeys = qstring["items"]
